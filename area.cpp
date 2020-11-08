62030cade5c6de9a14a7f9d9aabb9165d1b0ff5b
Cyril K. U <71171421+cyril1010@users.noreply.github.com>
2020-11-08 12:47:51 +0530

    AREA OF RECTANGLE / CIRCLE / TRIANGLE

---
+++ area.cpp
@@ -0,0 +1,29 @@
+//AREA OF RECTANGLE / CIRCLE / TRIANGLE USING SWITCH STATEMENT IN C++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l_rect,b_rect,a_rect,r_circle,a_circle,b_tri,h_tri,a_tri;
+    int ch;
+    cout<<" ENTER YOUR CHOICE = ";
+    cout<<" \n 1.Rectangle \n 2.Circle \n 3. Triangle \n";
+    cin>>ch;
+    switch(ch)
+    {
+        case 1 : cout<<" ENTER THE LENGTH AND BREADTH = ";
+        cin>>l_rect>>b_rect;
+        cout<<" \n AREA OF THE RECTANGLE = "<<l_rect*b_rect;
+		break;
+		case 2 : cout<<" ENTER THE RADIUS = ";
+		cin>>r_circle;
+		cout<<" \n AREA OF THE CIRCLE = "<<3.14*r_circle*r_circle;
+		break;
+		case 3 : cout<<" ENTER THE BREADTH AND HEIGHT = ";
+		cin>>b_tri>>h_tri;
+		cout<<" \n AREA OF THE TRIANGLE = "<<(b_tri*h_tri)/2;
+		break;
+    }
+return 0;
+}
+
